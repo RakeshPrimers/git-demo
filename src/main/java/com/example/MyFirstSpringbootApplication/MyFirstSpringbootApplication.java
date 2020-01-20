aa0ccfafc59019c94a9412ebedadc450d4862838
RakeshPrimers <tadepu143@gmail.com>
2020-01-20 16:38:23 +0530

    added project to local repo-second time

--- src/main/java/com/example/MyFirstSpringbootApplication/MyFirstSpringbootApplication.java
+++ src/main/java/com/example/MyFirstSpringbootApplication/MyFirstSpringbootApplication.java
@@ -6,19 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MyFirstSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstSpringbootApplication.class, args);
 	}
 
-	@RequestMapping("/mydear")
-	public String Hello()
-	{
-		
-		System.out.println("hello Git Exampl");
-		System.out.println("hello Git Exampl");
-		return "Heyy SpringBoot";
-	}
+	
 }
---
+++ src/main/java/com/example/MyFirstSpringbootApplication/controllers/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.MyFirstSpringbootApplication.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	
+	@RequestMapping("/hellogit")
+	public String HelloMessage()
+	{
+		System.out.println("hello My dear Git");
+		return "hello mama Git";
+	}
+}
