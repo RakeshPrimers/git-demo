cf7c452e593bf6050c1ad754a3cd7a00a9857e7e
RakeshPrimers <tadepu143@gmail.com>
2020-01-20 17:38:38 +0530

    devlopment branch

--- src/main/java/com/example/MyFirstSpringbootApplication/controllers/DemoController.java
+++ src/main/java/com/example/MyFirstSpringbootApplication/controllers/DemoController.java
@@ -6,15 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	
 	@RequestMapping("/hellogit")
-	public String HelloMessage()
-	{
+	public String HelloMessage() {
 		System.out.println("hello My dear Git");
 		System.out.println("hello My dear Git");
 		System.out.println("hello My dear Gi22");
 		System.out.println("hello Added in Github repository remotely");
-                System.out.println("hello My dear added from Developer 2 locally");
+		System.out.println("hello My dear added from Developer 2 locally");
+		System.out.println("hello by using Branch Devlopment");
 		return "hello mama Git";
 	}
 }
